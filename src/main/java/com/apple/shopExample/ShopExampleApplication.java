3caa84fb23bbf77c6ea4a0b950c378501775fd97
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2025-02-28 15:35:05 +0900

    Initial commit

---
+++ src/main/java/com/apple/shopExample/ShopExampleApplication.java
@@ -0,0 +1,14 @@
+package com.apple.shopExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopExampleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShopExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apple/shopExample/ShopExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apple.shopExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
