2f22ae6b017380fe32fe14aaf10e7b276b011852
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2025-03-05 21:24:01 +0900

    [FEAT] Azure DB 서버 설정 및 연결과 JPA 설치

--- src/main/java/com/apple/shopExample/ItemController.java
+++ src/main/java/com/apple/shopExample/ItemController.java
@@ -11,6 +11,7 @@ public class ItemController {
     @GetMapping("/list")
     String list(Model model){
         model.addAttribute("name","홍길동");
+        model.addAttribute("name","비싼 바지");
         return "list.html";
     }
 }
