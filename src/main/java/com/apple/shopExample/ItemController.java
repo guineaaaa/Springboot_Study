562ed41831fc54621bd20b570069a0e55ffae577
guineaaaa <165776804+guineaaaa@users.noreply.github.com>
2025-03-05 22:28:11 +0900

    [FEAT] Lombok 설정과 DB입출력

--- src/main/java/com/apple/shopExample/ItemController.java
+++ src/main/java/com/apple/shopExample/ItemController.java
@@ -1,16 +1,33 @@
 package com.apple.shopExample;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
 
+    private final ItemRepository itemRepository;
+
+    /** @Autowired - Lombok 없이 등록 newItemRepository()하나 뽑아서 변수에 넣음
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+     */
+
     @GetMapping("/list")
+
     String list(Model model){
-        model.addAttribute("name","홍길동");
+        List<Item> result=itemRepository.findAll(); //itemobject 형태로 모든행가져오기
+        System.out.println(result.get(0)); // item 클래스의 Object
+        System.out.println(result.get(0).price); //첫번째 데이터의 price
+        System.out.println(result.get(0).title); //첫번째 데이터의 title
+
         model.addAttribute("name","비싼 바지");
         return "list.html";
     }
---
+++ src/main/java/com/apple/shopExample/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.apple.shopExample;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long>{
+
+}
+
